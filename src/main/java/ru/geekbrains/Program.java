4185b12729239e3af67d5af2d9a3fe92d26b70ce
Alexey Zotov <Alexey.Zotov@ymail.com>
2024-02-22 12:08:23 +0300

    Comments added to the classes

--- src/main/java/ru/geekbrains/Program.java
+++ src/main/java/ru/geekbrains/Program.java
@@ -1,5 +1,8 @@
 package ru.geekbrains;
 
+/**
+ * Entry point for the program
+ */
 public class Program {
     public static void main(String[] args) {
         TicTacToeGame ticTacToeGame = new TicTacToeGame(5, 3);
--- src/main/java/ru/geekbrains/TicTacToeGame.java
+++ src/main/java/ru/geekbrains/TicTacToeGame.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * Simple ticTacToe game designed as a solution to second homework
+ */
 public class TicTacToeGame {
     private final Random random = new Random();
     private final Scanner scanner = new Scanner(System.in);
@@ -15,24 +18,42 @@ public class TicTacToeGame {
     private int turnsCount = 0;
     private final int repeatsInLineToWin;
 
+    /**
+     * Simple constructor which implements default length of win combination
+     * which equals to field size
+     * @param fieldSize the size of the square field
+     */
     public TicTacToeGame(int fieldSize) {
         this.fieldSize = fieldSize;
         repeatsInLineToWin = fieldSize;
         startGame();
     }
 
+    /**
+     * Custom constructor
+     * @param fieldSize the size of the square field
+     * @param repeatsInLineToWin custom length of repeats to win
+     */
     public TicTacToeGame(int fieldSize, int repeatsInLineToWin){
         this.fieldSize = fieldSize;
         this.repeatsInLineToWin = repeatsInLineToWin;
         startGame();
     }
 
+    /**
+     * EntryPoint method for the game to start
+     */
     private void startGame() {
         boolean isEasyLevel = getAiLevel();
         initializeField();
         printField();
         HashMap<Integer, Integer> activeTurn;
         int gameStatus;
+
+        if (!isEasyLevel) {
+            System.out.println("Warning! Hard level is not implemented. Switching to easy level");
+            isEasyLevel = true;
+        }
 
         while (true){
             activeTurn = userTurn();
@@ -57,6 +78,11 @@ public class TicTacToeGame {
         }
     }
 
+    /**
+     * Console messenger to inform the user about game status
+     * @param gameStatus digit code which receive 1, 2, 3 value to determine if player win or AI
+     *                   or it a draw in a game
+     */
     private void printGameStatus(int gameStatus){
         if(gameStatus == 3){
             System.out.println("There is a draw!");
@@ -65,6 +91,10 @@ public class TicTacToeGame {
         System.out.println(gameStatus == 1 ? "Player Win" : "AI Wins");
     }
 
+    /**
+     * Console user interface to interact with user to get the preferred level of AI strength
+     * @return return true if easy level selected
+     */
     private boolean getAiLevel() {
         System.out.println("Welcome to TicTacToeGame!\nPlease select level to start the game\n1. Easy\n2.Hard\nEnter your choose:");
         boolean isSelectionMade = false;
@@ -82,6 +112,9 @@ public class TicTacToeGame {
         return userInput.equals("1");
     }
 
+    /**
+     * Method to fill the field with empty markers on game start
+     */
     private void initializeField() {
         gameField = new char[this.fieldSize][this.fieldSize];
 
@@ -92,6 +125,9 @@ public class TicTacToeGame {
         }
     }
 
+    /**
+     * Method to print the game to the console
+     */
     private void printField() {
         for (int i = 0; i < fieldSize; i++) {
             System.out.print(i + 1 + "|");
@@ -108,6 +144,11 @@ public class TicTacToeGame {
         System.out.println();
     }
 
+    /**
+     * Method to interact with user and get both x and y coordinates
+     * Occupied cells check implemented to avoid incorrect turns
+     * @return Map with x and y coordinates with x as key
+     */
     private HashMap<Integer, Integer> userTurn() {
         int x = 0;
         int y = 0;
@@ -132,6 +173,10 @@ public class TicTacToeGame {
         return turn;
     }
 
+    /**
+     * AI turns generator for easy level of AI with checks for occupied cells turns
+     * @return Map with x and y coordinates with x as key
+     */
     private HashMap<Integer, Integer> aiEasyTurn(){
         HashMap<Integer, Integer> turn = new HashMap<>();
         boolean isEmptyCell = false;
@@ -151,6 +196,11 @@ public class TicTacToeGame {
         return turn;
     }
 
+    /**
+     * supporting method to check if it is draw win or loose for player
+     * @param turn receiving last turn as a Map with x and y coordinates with x as key
+     * @return code value there 1:player wins, 2: AI wins and 3: is Draw
+     */
     private Integer checkGameStatus(HashMap<Integer, Integer> turn){
         int result;
         int noCombination = 0;
@@ -166,14 +216,24 @@ public class TicTacToeGame {
         return checkDiagonalRightLeft(turn);
     }
 
-    private Integer checkHorizontal(HashMap<Integer, Integer> userTurn){
-        int xPosition = userTurn.keySet().iterator().next();
-        int yPosition = userTurn.get(xPosition);
+    /**
+     * Method to check if it is a horizontal combination from the last turn
+     * @param turn receiving last turn as a Map with x and y coordinates with x as key
+     * @return code value there 1:player wins, 2: AI wins and 3: is Draw
+     */
+    private Integer checkHorizontal(HashMap<Integer, Integer> turn){
+        int xPosition = turn.keySet().iterator().next();
+        int yPosition = turn.get(xPosition);
         char[] combination = new char[fieldSize];
         System.arraycopy(gameField[yPosition], 0, combination, 0, fieldSize);
         return checkCombination(combination);
     }
 
+    /**
+     * Method to check if it is a vertical combination from the last turn
+     * @param turn receiving last turn as a Map with x and y coordinates with x as key
+     * @return code value there 1:player wins, 2: AI wins and 3: is Draw
+     */
     private Integer checkVertical(HashMap<Integer, Integer> turn){
         int xPosition = turn.keySet().iterator().next();
         char[] combination = new char[fieldSize];
@@ -184,6 +244,11 @@ public class TicTacToeGame {
         return checkCombination(combination);
     }
 
+    /**
+     * Method to check if it is a diagonal combination from the last turn
+     * @param turn receiving last turn as a Map with x and y coordinates with x as key
+     * @return code value there 1:player wins, 2: AI wins and 3: is Draw
+     */
     private Integer checkDiagonalLeftRight(HashMap<Integer, Integer> turn){
         HashMap<Integer, Integer> initialPosition = getStartPointDiagonal(turn, true);
         int xPosition = initialPosition.keySet().iterator().next();
@@ -201,6 +266,11 @@ public class TicTacToeGame {
         return checkCombination(combination);
     }
 
+    /**
+     * Method to check if it is a diagonal combination from the last turn
+     * @param turn receiving last turn as a Map with x and y coordinates with x as key
+     * @return code value there 1:player wins, 2: AI wins and 3: is Draw
+     */
     private Integer checkDiagonalRightLeft(HashMap<Integer, Integer> turn){
         HashMap<Integer, Integer> initialPosition = getStartPointDiagonal(turn, false);
         int xPosition = initialPosition.keySet().iterator().next();
@@ -218,6 +288,12 @@ public class TicTacToeGame {
         return checkCombination(combination);
     }
 
+    /**
+     * Support method to search the start point of diagonals
+     * @param currentPosition Map with x and y coordinates with x as key of the last turn
+     * @param isLeftToRight flag to change the logic for the initial position for combination to search
+     * @return Map with x and y coordinates with x as key
+     */
     private HashMap<Integer, Integer> getStartPointDiagonal(HashMap<Integer, Integer> currentPosition, boolean isLeftToRight){
         int xPosition = currentPosition.keySet().iterator().next();
         int yPosition = currentPosition.get(xPosition);
@@ -251,6 +327,11 @@ public class TicTacToeGame {
         return initialPosition;
     }
 
+    /**
+     * Support method to determine if there is a repeat in a sequence
+     * @param combination passed to method combination which need to be checked for repeats
+     * @return Integer as code for win status or no combinations
+     */
     private Integer checkCombination(char[] combination) {
         int counter = 0;
         int noCombination = 0;
@@ -276,6 +357,12 @@ public class TicTacToeGame {
         return noCombination;
     }
 
+    /**
+     * Support method to get coordinated from user
+     * through the console with checks if it is an integer and if the value in a boundaries of the field
+     * @param message custom message prior to user enter the value
+     * @return desired coordinate
+     */
     private Integer getCoordinateFromUser(String message){
         boolean isCorrectValue = false;
         int value = 0;
@@ -299,6 +386,10 @@ public class TicTacToeGame {
         return value;
     }
 
+    /**
+     * method to check if possible steps available
+     * @return true if draw
+     */
     private boolean isDraw(){
         return turnsCount == fieldSize * fieldSize;
     }
